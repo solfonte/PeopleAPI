4d52669633fa1b8296e0d062cab33dc1b5625f39
solfonte <msfontenla@fi.uba.ar>
2023-02-19 09:47:28 -0300

    fix: moved database settings class

--- Models/PeopleDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Models;
-
-public class PeopleDatabaseSettings {
-    public string ConnectionString { get; set; } = null!;
-
-    public string DatabaseName { get; set; } = null!;
-
-    public string PersonCollectionName { get; set; } = null!;
-}
