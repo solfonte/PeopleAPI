fe413494074fbb87e56ea8bff51cfc5353259c6d
solfonte <msfontenla@fi.uba.ar>
2023-02-20 15:54:24 -0300

    fix: removed unused route

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -4,7 +4,6 @@ const { env } = require('process');
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:13026';
 
 const context = [
-  "/weatherforecast",
   "/person"
 ];
 
