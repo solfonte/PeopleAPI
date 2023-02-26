f8fa8bebf935067ea6c2f87abf4cd472bb33ea68
solfonte <msfontenla@fi.uba.ar>
2023-02-26 16:25:43 -0300

    refactor: removed unused lines

--- ClientApp/src/components/ConfirmDialog.js
+++ ClientApp/src/components/ConfirmDialog.js
@@ -2,30 +2,13 @@ import { Dialog, DialogContent, DialogTitle, DialogActions } from "@mui/material
 import Typography from '@mui/material/Typography';
 import Button from "@mui/material/Button";
 import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
-//import { makeStyles } from "@mui/styles";
-
-/*
-const useStyles = makeStyles(theme => ({
-    dialog: {
-        padding: theme.spacing(2),
-        position: 'absolute',
-        top: theme.spacing(5),
-    },
-    dialogContent: {
-        textAlign: 'center'
-    },
-    dialogAction: {
-        justifyContent: 'center'
-    }
-}))
-*/
 
 export default function ConfirmDialog (props){
     const {title, subtitle, color, confirmDialog, setConfirmDialog} = props;
-   // const classes = useStyles()
+
     return (
-        <Dialog open={confirmDialog.isOpen} /*classes={{paper:classes.dialog}}*/>
-            <ReportGmailerrorredIcon/>
+        <Dialog open={confirmDialog.isOpen}>
+            <ReportGmailerrorredIcon fontSize="large"/>
             <DialogTitle>
             </DialogTitle>
             <DialogContent>
@@ -36,7 +19,7 @@ export default function ConfirmDialog (props){
                     {confirmDialog.subtitle}
                 </Typography>
             </DialogContent>
-            <DialogActions /*classes={{paper:classes.dialogAction}}*/>
+            <DialogActions>
                 <Button color="primary" variant="contained" onClick={confirmDialog.onConfirm}>Yes</Button>
                 <Button color="primary" variant="outlined" onClick={() => setConfirmDialog({...confirmDialog, isOpen:false})}>No</Button>
             </DialogActions>
