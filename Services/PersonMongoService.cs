e6c4d924e1b9d0935c886aee54750e79550ede1f
solfonte <msfontenla@fi.uba.ar>
2023-02-27 09:48:03 -0300

    feat: add method to person service

--- Services/PersonMongoService.cs
+++ Services/PersonMongoService.cs
@@ -29,7 +29,7 @@ public class PersonMongoService : IPersonService {
         await _personCollection.InsertOneAsync(person);
     }
 
-    public async Task<Person> getPersonWithNationalID(ulong nationalID){
+    public async Task<Person> getPersonWithNationalID(string nationalID){
         return await _personCollection.Find(Builders<Person>.Filter.Eq("NationalID", nationalID)).Limit(1).SingleAsync();
     }
 
